ee5a50703e10b4044fdb5df041070f43b83123a4
Julien RIALLAND <julien.rialland@groupe-asten.fr>
2024-07-05 17:44:15 +0200

    handle path prefix correctly, use etree for generating propfind responses

--- tests/server/__main__.py
+++ tests/server/__main__.py
@@ -7,10 +7,10 @@ import uvicorn
 def main():
     parser = ArgumentParser()
     parser.add_argument("--host", default="127.0.0.1")
-    parser.add_argument("--port", default=8080, type=int)
+    parser.add_argument("--port", default=8000, type=int)
     args = parser.parse_args()
 
-    uvicorn.run(DAVApp(OSFS(".")), host=args.host, port=args.port)
+    uvicorn.run(DAVApp(OSFS("."), path_prefix='/WEBDAV'), host=args.host, port=args.port)
 
 
 main()
